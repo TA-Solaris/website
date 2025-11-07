92a6cdf874973893318b3e8686352046cc8d99c4
Edward Potter <pottered2@gmail.com>
2025-11-07 18:58:38 +0000

    Theming

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Post from "./pages/Post";
+import { ThemeProvider } from "@/components/theme-provider"
 
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/post/:slug" element={<Post />} />
-      </Routes>
-    </Router>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/post/:slug" element={<Post />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   )
 }
 
